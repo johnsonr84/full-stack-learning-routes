3fa447c543a9d196d4332e1634b03d0c432d657a
Your Name <robertcjohnson1984@gmail.com>
2022-01-12 22:56:26 -0700

    create pages folder and add home page

---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Javascript from '../../components/diagrams/JavaScript/JavaScript'
+
+const Home = () => {
+  return (
+    <>
+      <div className="js-diagram" style={{ height: '60vh' }}>
+        <Javascript />
+      </div>
+    </>
+  )
+}
+
+export default Home
